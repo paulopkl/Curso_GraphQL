7c1fbccf219b8d2d74cdcdcd734abc436420fc06
Paulo Ricardo <palmeida.ipms@gmail.com>
2021-01-31 00:56:25 -0300

    insert file created

--- Data-base/tests/insert.js
+++ Data-base/tests/insert.js
@@ -1,10 +1,26 @@
+const db = require('../config/db');
 const knex = require('../config/db');
 
-const newProfile = {
-    name: 'register',
-    label: 'Register'
+// const newProfile = {
+//     name: 'stranger',
+//     label: 'Stranger'
+// }
+
+// knex('profiles')
+//     .insert(newProfile)
+//         .then(res => console.log(res))
+//         .catch(err => console.error(err.sqlMessage))
+//         .finally(() => db.destroy());
+
+const profileRT = {
+    name: 'root' + Math.random(),
+    label: "Super User chmod+777"
 }
 
-knex('profiles')
-    .insert(newProfile)
-    .then(res => console.log(res))
+// INSERT INTO profiles (name, label) VALUES ("...", "...")
+db.insert(profileRT).into('profiles')
+    .then(res => res[0])
+    .then(id => `The generated Id was ${id}`)
+    .then(string => console.log(string))
+    .catch(err => console.error(err.sqlMessage))
+    .finally(() => db.destroy());
---
+++ Data-base/tests/queries.js
@@ -0,0 +1,29 @@
+const db = require('../config/db');
+
+// db('profiles')
+//     // .map(p => p.name)
+//     .then(res => res.map(p => p.name))
+//     .then(names => console.log(names))
+//     .finally(() => db.destroy());
+    
+// db('profiles')
+//     .select('name', 'id')
+//         .then(res => console.log(res))
+//         .finally(() => db.destroy());
+
+// db.select('name', 'id').from('profiles')
+//     .limit(4)
+//     .offset(1)
+//         .then(res => console.log(res))
+//         .finally(() => db.destroy());
+
+db('profiles')
+    .select('id', 'name')
+    // .where({ id: 2 }) // Where Id it's equals to 2
+    // .where("id", "=", 2)
+    // .where("name", "like", "%a%")
+    // .whereNot({ id: 2 })
+    .whereIn('id', [1, 2, 3])
+    // .first() // Pick the first element of an Array
+        .then(res => console.log(res))
+        .finally(() => db.destroy());
